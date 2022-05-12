d65fc7b64c13b85697cfe7da583947524adc8b98
Jorge Rojas <exxscher@gmail.com>
2022-05-11 20:14:44 -0400

    nuevos cambios

--- prueba.js
+++ prueba.js
@@ -1,5 +1,5 @@
-const palabra = "cambio de jorge"
-console.log(asdasd2)
+const palabra2 = "cambio de jorge"
+console.log(palabra2)
 const asdasd2 = "probando merge" + " otra cosa"
 
 // comentario lore
